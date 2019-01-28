6a69fab536d7ab636fd2371adb7d6da67bea9160
khengschew <khengc@gmail.com>
2019-01-28 14:08:31 -0800

    Implement piece adding to game board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,34 @@
 var board = {
   // MODEL CODE
+  players: [
+    {char: 'X', color: 'red'},
+    {char: 'O', color: 'green'}
+  ],
+  turnCount: 0,
+  checkWin: function() {
 
+  },
 
   // CONTROLLER CODE
+  toggle: function(e) {
+    // Update clicked div
+    var valueDiv = document.getElementById(e.target.dataset.rowid + e.target.dataset.colid);
 
+    if (valueDiv && valueDiv.innerHTML.trim() === '') {
+      // Increment turn
+      board.turnCount += 1;
+      var currentTurn = board.players[board.turnCount % 2];
+
+      // Add symbol to board
+      valueDiv.innerHTML = `<div style="color:${currentTurn.color}; width:80%; height:80%; font-size:80px; display:flex; align-items:center; justify-content:center;">${currentTurn.char}</div>`;
+    }
+  },
 
   // VIEW CODE
-  // Generate Board
   initialize: function() {
-    console.log('Board is initialized!');
+    console.log('Board is initializing...');
 
+    // Generate Board
     // Loop through row, 0-2:
       // Loop through column, 0-2:
         // Create div with fixed size and width, class = 'grid'
@@ -19,14 +38,24 @@ var board = {
     for (var r = 0; r < 3; r++) {
       for (var c = 0; c < 3; c++) {
         gameBoard += `
-          <div class="grid" data-row="${r}" data-col="${c}" style="border:solid; border-width:1px;">
-            <div class="xo" id="${JSON.stringify(r) + JSON.stringify(c)}">
-            </div>
+          <div
+            class="grid"
+            id="${JSON.stringify(r) + JSON.stringify(c)}"
+            data-rowId="${r}"
+            data-colId="${c}"
+            style="border:solid; border-width:1px; display:flex; align-items:center; justify-content:center;"
+            onClick="board.toggle(event)"
+          >
           </div>
         `;
       }
     }
 
     document.getElementById('gameBoard').innerHTML = gameBoard;
+
+    // Add onClick handlers
+    document.getElementById('resetButton').onclick = board.initialize;
+
+    console.log('Initialized!');
   }
 }
