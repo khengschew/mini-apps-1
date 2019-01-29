7c8cc28b9a5181bd1e0a02230c26bf2f39900f50
khengschew <khengc@gmail.com>
2019-01-29 11:50:42 -0800

    Set up express on server, add form to index.html, connect basic form handler

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
+var app = {
+  postJSON: function(e) {
+    e.preventDefault();
+    // Send JSON to server
+    // Clear JSON
+    // Set result div
+    console.log(e.target.value);
+  },
 
+  initialize: function() {
+    // Attach handlers
+    document.getElementById('submitForm').onclick = (event) => app.postJSON(event);
+  }
+};
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,6 @@
-
-
+// Express setup
+var express = require('express');
+var app = express();
+app.set('port', 3000);
+app.use(express.static(__dirname + '/client'));
+app.listen(process.env.PORT || app.get('port'));
