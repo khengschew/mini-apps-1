c1afd3e6a2e9eb2fe25899b368f4ba7924334dec
khengschew <khengc@gmail.com>
2019-01-30 11:29:52 -0800

    [challenge_3] Set up express and node, create skeleton index.html

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
 
+app.use(morgan('default'));
+app.use(express.static(__dirname + '/public'));
 
+app.set('port', 3000);
+app.listen(app.get('port'));
