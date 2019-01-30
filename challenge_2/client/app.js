c57d27c51987bbaf265c30095ec00d7788c0c5e9
khengschew <khengc@gmail.com>
2019-01-29 16:03:50 -0800

    Implement form POST submit, parse JSON and return csv values

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,15 +1,23 @@
 var app = {
   postJSON: function(e) {
     e.preventDefault();
-    // Send JSON to server
-    // Clear JSON
-    // Set result div
-    console.log(e.target.value);
+    var formData = document.getElementById('JSONData').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://127.0.0.1:3000', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        document.getElementById('JSONData').value = '';
+        document.getElementById('result').innerHTML = xhr.responseText;
+      }
+    };
+    xhr.send(formData);
   },
 
   initialize: function() {
     // Attach handlers
-    document.getElementById('submitForm').onclick = (event) => app.postJSON(event);
+    // document.getElementById('submitForm').onclick = (event) => app.postJSON(event);
   }
 };
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,5 +2,73 @@
 var express = require('express');
 var app = express();
 app.set('port', 3000);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(__dirname + '/client'));
 app.listen(process.env.PORT || app.get('port'));
+
+app.post('/', function(req, res) {
+  res.setHeader('Content-Type','text/plain');
+  res.send(jsonToCSV.parse(req.body));
+});
+
+jsonToCSV = {
+  parse: function(body) {
+    var headers = [];
+    var csvObjs = [];
+    var csvRows = [];
+    var jsonData = JSON.parse(body.JSONData);
+
+    // console.log(jsonData);
+
+    // Get object headers
+    // var headers = Object.keys(json);
+
+    // Loop through all keys:
+    // Get value for keys, insert into csvRow
+    // If children, then recursively call parse on children, append to results
+    var rParse = function(obj) {
+      var csvObj = {};
+
+      for (var key in obj) {
+        if (key !== 'children' && headers.includes(key) === false) {
+          headers.push(key);
+        }
+
+        if (key === 'children') {
+          for (var child of obj[key]) {
+            rParse(child);
+          }
+        } else {
+          csvObj[key] = obj[key];
+        }
+      }
+      csvObjs.push(csvObj);
+    };
+
+    rParse(jsonData);
+    console.log(csvObjs);
+    
+    // Loop through csvObjs:
+      // Loop through headers:
+        // Push value into csvRows[i]
+    // When done with loop(s), unshift headers into csvRows
+    for (var i = 0; i < csvObjs.length; i++) {
+      for (var j = 0; j < headers.length; j++) {
+        var currVal = csvObjs[i][headers[j]] || 'null';
+        if(csvRows[i]) {
+          csvRows[i].push(currVal);
+        } else {
+          csvRows[i] = [currVal];
+        }
+      }
+      csvRows[i] = csvRows[i].toString();
+    }
+    csvRows.unshift(headers);
+
+    return csvRows.join('\n');
+    // return csvRows;
+  }
+}
