eaae1fdddeba274b320eea06e3d2faa9be042ad1
khengschew <khengc@gmail.com>
2019-01-31 14:10:30 -0800

    Add webpack and adjust repo layout

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,8 @@
+var express = require('express');
+var morgan = require('morgan');
 
-
+var app = express();
+app.set('port', 3000);
+app.use(express.static(__dirname + '/client/dist/'));
+app.use(morgan('default'));
+app.listen(app.get('port'));
