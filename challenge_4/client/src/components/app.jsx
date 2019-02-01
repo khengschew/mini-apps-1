348ffc73a6f03151c9456c6b8c305c4d20eee611
khengschew <khengc@gmail.com>
2019-01-31 17:46:00 -0800

    Fix bug where clicking on filled row will still toggle player

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -37,19 +37,23 @@ class App extends React.Component {
 
   setPiece(e) {
     var col = e.target.dataset.y;
+    var piecePlaced = false;
 
     var updateBoard = this.state.board;
     for (var i = 0; i < this.state.rows; i++) {
       if (updateBoard[i][col] === 0) {
         updateBoard[i][col] = this.state.currPlayer;
+        piecePlaced = true;
         break;
       }
     }
 
-    this.setState({
-      board: updateBoard,
-      currPlayer: this.state.currPlayer === 1 ? 2 : 1
-    });
+    if (piecePlaced) {
+      this.setState({
+        board: updateBoard,
+        currPlayer: this.state.currPlayer === 1 ? 2 : 1
+      });
+    }
   }
 
   checkWin() {
@@ -60,7 +64,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Connect Four <button onClick={this.resetBoard}>New Game</button></h1>
-        <Board rows={6} cols={7} board={this.state.board} setPiece={this.setPiece}/>
+        <Board rows={this.state.rows} cols={this.state.cols} board={this.state.board} setPiece={this.setPiece}/>
       </div>
     );
   }
